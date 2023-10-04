d6c54787a123fab6e83a835525516d6dd351ef9e
Семен <marbledrop@yandex.ru>
2023-10-05 00:30:53 +0300

    git add Task_009

---
+++ Task_009/Program.cs
@@ -0,0 +1,15 @@
+﻿/*  Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+и показывает наибольшую цифру числа.
+78 -> 8 
+12 -> 2 
+85 -> 8 */
+
+int n = new Random().Next(10, 100); // [10,99]
+int n1 = n / 10;
+int n2 = n % 10;
+Console.WriteLine(n);
+
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
